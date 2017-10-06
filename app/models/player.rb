c9378bf94f50deb7f30fea5009280adfadc1ad42
vzaffalon <zaffalonvictor@gmail.com>
2017-10-06 04:29:18 -0300

    adding validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
   belongs_to :adventure
   belongs_to :user
+
+  validates_presence_of :nickname
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,7 @@
 class Session < ApplicationRecord
   belongs_to :adventure
+
+  validates_presence_of :date
+  validates_presence_of :title
+  validates_presence_of :adventure_id
 end
