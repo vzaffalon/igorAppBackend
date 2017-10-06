62c274746242c73b2876c8047afa8638b05c7110
vzaffalon <zaffalonvictor@gmail.com>
2017-10-06 04:13:37 -0300

    adding validations

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,4 +2,8 @@ class Adventure < ApplicationRecord
   belongs_to :user
   has_many :sessions
   has_many :players
+
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :user_id
 end
